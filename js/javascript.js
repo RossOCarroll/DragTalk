598a7274eabc4e0a070dec16f8acb2cb7a28ebc5
Ross O'Carroll <rossocarroll@gmail.com>
2026-01-19 11:12:38 -0800

    videos

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,11 @@
+const originalConsoleError = console.error;
+console.error = function(message, ...optionalParams) {
+  if (typeof message === 'string' && message.includes(location.hostname)) {
+    originalConsoleError(message, ...optionalParams);
+  }
+};
+
+
 class BandPage {
   constructor() {
     this.navBar = document.querySelectorAll('nav a');
@@ -13,7 +21,7 @@ class BandPage {
         link.classList.add('active');
         const url = link.getAttribute('href');
         this.loadSection(url);
-        this.resetNav(link);
+        this.resetNav(link);          
       })
     })
   }
@@ -22,12 +30,60 @@ class BandPage {
     try {
       const response = await fetch(url);
       if (!response.ok) throw new Error('Page not found');
+  
       const html = await response.text();
-      console.log(html);
-      this.mainContent.innerHTML = html;
-    } catch(err) {
-      console.error('Error loading page', err);
+      const doc = new DOMParser().parseFromString(html, 'text/html');
+      let content
+
+      if (url === 'videos-section.html') {
+        content = await this.loadVideos(doc.querySelector('section'))
+      } else {
+        content = doc.querySelector('section'); 
+      }
       
+  
+      this.mainContent.innerHTML = content.innerHTML;
+    } catch (err) {
+      console.error('Error loading page', err);
+    }
+  }
+
+  async loadVideos(section) {
+    try {
+      const response = await fetch('/data/videos.json');
+      if (!response.ok) throw new Error('Failed to fetch videos');
+
+      const videos = await response.json();
+      const grid = document.createElement('div');
+      grid.classList.add('video-grid');
+
+      videos.forEach(video => {
+        const videoEl = document.createElement('div');
+        videoEl.classList.add('video');
+  
+        const frame = document.createElement('div');
+        frame.classList.add('video-frame');
+  
+        const iframe = document.createElement('iframe');
+        iframe.src = video.src;
+        iframe.allowFullscreen = true;
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+
+        const h1 = document.createElement('h1');
+        h1.classList.add('video-title');
+        h1.textContent = video.title;
+  
+        frame.appendChild(iframe);
+        videoEl.appendChild(frame);
+        videoEl.appendChild(h1);
+        grid.appendChild(videoEl);
+      })
+
+      section.appendChild(grid);
+
+      return section;
+    } catch (err) {
+      console.error('Error loading videos', err);
     }
   }
 
