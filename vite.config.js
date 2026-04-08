0550de419bf60391f8ba1e0f44972579469e0bb9
Ross O'Carroll <rossocarroll@gmail.com>
2026-04-08 15:03:42 -0700

    another

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/DragTalk/', // GitHub Pages repo name
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: '/DragTalk/',
 });
