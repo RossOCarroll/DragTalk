2ce0a5fdc9771883f1e16e8744d3e5b8a70e99f4
Ross O'Carroll <rossocarroll@gmail.com>
2026-05-09 15:34:31 -0700

    TS - broken page

--- src/admin.js
+++ src/admin.js
@@ -6,7 +6,15 @@ function getSupabase() {
   if (!supabaseClient) {
     supabaseClient = createClient(
       'https://xnqvjcjmympojjtkhcmt.supabase.co',
-      'sb_publishable_poUZopim6HVLH-BycJrXag_NfIEh4Ft'
+      'sb_publishable_poUZopim6HVLH-BycJrXag_NfIEh4Ft',
+      {
+        auth: {
+          persistSession: true,
+          storageKey: 'drag-talk-auth',
+          detectSessionInUrl: false,
+          flowType: 'pkce'
+        }
+      }
     );
   }
   return supabaseClient;
